9618b301b0c00fc7198bb9ad4bc4ef94d34d5426
Gal <51763193+galelfer@users.noreply.github.com>
2020-06-22 21:59:46 +0300

    Update hw3_aux.cpp

--- hw3_aux.cpp
+++ hw3_aux.cpp
@@ -109,10 +109,7 @@ void symbol::decl_func(const string &name, const string &type, const string &ret
     CB.emit(input_llvm_stack_reg + " = alloca [" + input_size + " x i32]");
     llvm_stack_reg = freshVar();
     CB.emit(llvm_stack_reg + " = alloca [50 x i32]");
-    //TODO: open a new stack, with enough place for args + 50 (or 2 different stacks). - > emit("alloca..") (llvm).
-    // C++: update llvm_stack_reg to be it's register (and input_llvm_stack_reg as well?).
-    // recommanded: init NUM counter for FreshVar() so it won't overflow.
-    // put all vars in input_llvm_stack.
+
     for (int i = 0; i < (int)args.size(); i++) {
         const arg* tmp = get_var_type(args[i],types[i]);
         if(tmp!=nullptr){
@@ -122,7 +119,7 @@ void symbol::decl_func(const string &name, const string &type, const string &ret
         t_stack.back().emplace_back(arg(args[i], types[i], -i - 1));
         string address = freshVar();
         CB.emit(address + " = getelementptr [" + input_size + " x i32], [" + input_size + " x i32]* " + input_llvm_stack_reg + ",i32 0, i32 " + to_string(i));
-        CB.emit("store i32 " + address + ", i32* " + address);
+        CB.emit("store i32 " + tmp->name + ", i32* " + address); //TODO: change to tmp->reg or tmp->val...?
     }
 }
 
